796dc0c4094846c8f590f26845c35b2e02cc4e8c
Kedar Amladi <kamladi@gmail.com>
2014-07-11 12:28:06 -0700

    removing symlink check

--- sublime/setup-sublime.sh
+++ sublime/setup-sublime.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # setup_sublime
-DIR=~/Library/Application\ Support/Sublime\ Text\ 2
+DIR=~/Library/Application\ Support/Sublime\ Text\ 3
 
 # if user already has User package, back them up.
 if [ -d "$DIR/Packages/User" ]; then
-    echo "moving current (...Sublime Text 2)/Packages/User to (...Sublime Text 2)/Packages.bak/User"
+    echo "moving current (...Sublime Text)/Packages/User to (...Sublime Text)/Packages.bak/User"
     mkdir "$DIR/Packages.bak"
     mv "$DIR/Packages/User" "$DIR/Packages.bak/User"
 fi
@@ -13,8 +13,3 @@ fi
 # create symbolic link from $DOTFILES to DIR
 ln -s ~/dotfiles/sublime/User "$DIR/Packages/User"
 
-# finally, add subl command to terminal path  if necessary
-if[ ! -L /usr/bin/subl ]; then
-	echo "adding sublime command to terminal path"
-	ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/bin/subl
-fi
