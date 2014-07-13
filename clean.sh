f602f644a64727c7927fa608952733802177165a
Kedar Amladi <kamladi@gmail.com>
2014-07-12 18:05:14 -0700

    added clean script and util script

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. ./util.sh
+
+e_header "Cleaning up..." 
+
+files=( ".aliases" ".bash_profile" ".vim" ".vimrc" ".zshrc" ".oh-my-zsh" )
+for file in "${files[@]}"; do
+    path="$HOME/$file"
+    e_header "Removing $path" 
+    rm $path
+done
+
+e_success "Cleanup complete." 
+
